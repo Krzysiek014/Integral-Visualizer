71d4402da19e20e1a941d48c995a4515fe35d42c
Kamil Mikołajczuk <kamil.mikolajczuk@comarch.pl>
2020-12-08 19:06:19 +0100

    Parser

--- Parser/ParserWyrazen.py
+++ Parser/ParserWyrazen.py
@@ -1,5 +1,5 @@
+from Parser.Wartosci.Zmienna import X, Y
 from Parser.Wartosci.Liczba import Liczba, E, PI
-from Parser.Wartosci.Zmienna import Zmienna
 
 from Parser.Operacje.Arytmetyczne.Dodawanie import DodawanieOperator
 from Parser.Operacje.Arytmetyczne.Odejmowanie import OdejmowanieOperator
@@ -10,8 +10,9 @@ from Parser.Operacje.Trygonometryczne.Sinus import SinusOperator
 from Parser.Operacje.Trygonometryczne.Cosinus import CosinusOperator
 
 class ParserWyrazen:
-  operatory = []
-  dzialania = [
+  stale = [PI, E]
+  zmienne = [X, Y]
+  operatory = [
     DodawanieOperator(),
     OdejmowanieOperator(),
     MnozenieOperator(),
@@ -21,48 +22,21 @@ class ParserWyrazen:
     CosinusOperator(),
   ]
 
-  def parsuj(self, wyrazenie: str):
-    tokeny = wyrazenie.lower().split(" ")
+  def parsuj(self, wyrazenie: str): 
     stos = []
 
-    for token in tokeny:
-      czyOperand = False
+    for token in wyrazenie.lower().split(" "):
+      if self._parsujDzialania(token, stos):
+        continue
 
-      for op in self.dzialania:
-        if op.akceptuje(token):
-          iloscArgumentow = 2 if op.czyDwuargumentowy else 1
+      if self._parsujZmienne(token, stos):
+        continue
 
-          if len(stos) < iloscArgumentow:
-            raise Exception(f"Niepoprawne wyrażenie za mało argumentów dla operatora { token } oczekiwano { iloscArgumentow } jest { len(stos) }")
+      if self._parsujStale(token, stos):
+        continue
 
-          czyOperand = True
+      self._parsujLiczbe(token, stos)
 
-          if op.czyDwuargumentowy:
-            b = stos.pop()
-            a = stos.pop()
-
-            stos.append(op.utworz(a, b))
-          else:
-            a = stos.pop()
-
-            stos.append(op.utworz(a, a))
-
-          break
-      
-      if not czyOperand:
-        if token in ['x', 'y']:
-          stos.append(Zmienna(token))
-        elif token == 'e':
-          stos.append(E)
-        elif token == 'pi':
-          stos.append(PI)
-        else:
-          try:
-            wartosc = float(token)
-            stos.append(Liczba(wartosc))
-          except:
-            raise Exception(f"Oczekiwano wartości liczbowej spotkano { token }")
-        
     if len(stos) > 1:
       raise Exception(f"Niepoprawne wyrażenie za mało operatorów, brakuje { len(stos) - 1 }")
 
@@ -70,4 +44,48 @@ class ParserWyrazen:
       raise Exception("Puste wyrażenie")
     
     return stos[0]
+  
+  def _parsujDzialania(self, token: str, stos: list):
+    for operator in self.operatory:
+      if operator.akceptuje(token):
+        iloscArgumentow = 2 if operator.czyDwuargumentowy else 1
 
+        if len(stos) < iloscArgumentow:
+          raise Exception(f"Niepoprawne wyrażenie za mało argumentów dla operatora { token } oczekiwano { iloscArgumentow } jest { len(stos) }")
+
+        if operator.czyDwuargumentowy:
+          b = stos.pop()
+          a = stos.pop()
+          stos.append(operator.utworz(a, b))
+        else:
+          a = stos.pop()
+          stos.append(operator.utworz(a, a))
+
+        return True
+
+    return False
+
+  def _parsujZmienne(self, token: str, stos: list):
+    for zmienna in self.zmienne:
+      if str(zmienna) == token:
+        stos.append(zmienna)
+        return True
+
+    return False
+
+  def _parsujStale(self, token: str, stos: list):
+    for stala in self.stale:
+      if str(stala) == token:
+        stos.append(stala)
+        return True
+
+    return False
+
+  def _parsujLiczbe(self, token: str, stos: list):
+    try:
+      stos.append(Liczba(float(token)))
+      return True
+    except:
+      raise Exception(f"Oczekiwano wartości liczbowej spotkano { token }")
+    
+    return False
